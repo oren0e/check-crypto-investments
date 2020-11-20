cad5da329d039338d0f43aef83ac0f42673a9c87
Oren Epshtain <oren@gojuno.com>
2020-11-20 03:20:24 +0200

    remove unnessesary lines from cci.

--- cci.py
+++ cci.py
@@ -1,8 +1,5 @@
-from backend.data_tools import CoinSearch, DataHandler
+from backend.data_tools import DataHandler
 
 if __name__ == '__main__':
-    cs = CoinSearch()
-    cs.search_for_coin('snx')
-
     dh = DataHandler()
     dh.calculate_returns()
