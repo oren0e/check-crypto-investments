8dc67ae25d3e62d7f27085eeca37218ccc7ec9e7
Oren Epshtain <oren@gojuno.com>
2020-11-22 10:11:30 +0200

    start edit the tests.

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -1,5 +1,7 @@
 from backend.data_tools import DataHandler, CoinSearch
 
+from unittest import mock
+
 import pytest
 
 
@@ -13,4 +15,6 @@ def dh() -> DataHandler:
     return DataHandler()
 
 
-# TODO: write test on each part of the flow
+@mock.patch("backend.data_tools.DataHandler._get_prices")
+def test_calculate_returns(mock_initial_dict: mock.Mock, dh) -> None:
+    mock_initial_dict.return_value = {'coin_A': 100, 'coin_B': 400, 'coin_C': 10.12}
