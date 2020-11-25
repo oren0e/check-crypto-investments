37fef1ea336b57f554995b9558fc43d47ecfb616
Oren Epshtain <oren@gojuno.com>
2020-11-25 15:23:16 +0200

    test for calc returns works.

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -1,8 +1,10 @@
-from backend.data_tools import DataHandler, CoinSearch
+from backend.data_tools import DataHandler, CoinSearch, initial_dict
 
 from unittest import mock
 
 import pytest
+
+from typing import Dict
 
 
 @pytest.fixture
@@ -15,10 +17,14 @@ def dh() -> DataHandler:
     return DataHandler()
 
 
-@mock.patch.dict('backend.data_tools.initial_dict', {'coin_A': 10, 'coin_B': 20})
-@mock.patch("backend.data_tools.DataHandler.CoinGeckoAPI.get_price")
-def test_calculate_returns(mock_new_prices, initial_dict, dh) -> None:
+@mock.patch('backend.data_tools.initial_dict')
+@mock.patch('backend.data_tools.telegram_send')
+@mock.patch("backend.data_tools.CoinGeckoAPI.get_price")
+def test_calculate_returns(mock_new_prices, tele_mock, initial_dict_mock, dh) -> None:
     mock_new_prices.return_value = {'coin_A': {'usd': 50}, 'coin_B': {'usd': 200}}
-    with mock.patch('backend.data_tools.telegram_send') as tele_mock:
-        dh.calculate_returns()
-        tele_mock.assert_has_calls([mock.call('Return for coin_A: 400.0%'), mock.call('Return for coin_A: 900.0%')])
+    d: Dict[str, float] = {'coin_A': 10, 'coin_B': 20}
+    initial_dict_mock.__getitem__.side_effect = d.__getitem__
+    initial_dict_mock.__iter__.side_effect = d.__iter__
+    initial_dict_mock.items.side_effect = d.items
+    dh.calculate_returns()
+    tele_mock.assert_has_calls([mock.call('Return for coin_A: 400.0%'), mock.call('Return for coin_B: 900.0%')])
