c6f65a1380e38b3b2b82c4c30285fa9abb16c0cf
Oren Epshtain <oren@gojuno.com>
2020-11-25 13:47:26 +0200

    update tests.

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -16,7 +16,7 @@ def dh() -> DataHandler:
 
 
 @mock.patch.dict('backend.data_tools.initial_dict', {'coin_A': 10, 'coin_B': 20})
-@mock.patch("backend.data_tools.DataHandler.cg.get_price")
+@mock.patch("backend.data_tools.DataHandler.CoinGeckoAPI.get_price")
 def test_calculate_returns(mock_new_prices, initial_dict, dh) -> None:
     mock_new_prices.return_value = {'coin_A': {'usd': 50}, 'coin_B': {'usd': 200}}
     with mock.patch('backend.data_tools.telegram_send') as tele_mock:
