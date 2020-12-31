a9639864755ad693f76b3b25f7c9d1994a75ce5a
Oren Epshtain <oren@gojuno.com>
2020-12-31 21:19:56 +0200

    add check for env vars for telegram

--- utils/telegram.py
+++ utils/telegram.py
@@ -2,10 +2,15 @@ import os
 
 import requests
 
-API_TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
-CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
+from typing import Optional
+
+API_TOKEN: Optional[str] = os.environ.get('TELEGRAM_API_TOKEN')
+CHAT_ID: Optional[str] = os.environ.get('TELEGRAM_CHAT_ID')
 
 
 def telegram_send(message: str) -> None:
-    send_msg = 'https://api.telegram.org/bot' + API_TOKEN + '/sendMessage?chat_id=' + CHAT_ID + '&parse_mode=Markdown&text=' + message
-    requests.get(send_msg)
+    if API_TOKEN and CHAT_ID:
+        send_msg = 'https://api.telegram.org/bot' + API_TOKEN + '/sendMessage?chat_id=' + CHAT_ID + '&parse_mode=Markdown&text=' + message
+        requests.get(send_msg)
+    else:
+        raise RuntimeError("Missing Telegram token or chat id")
