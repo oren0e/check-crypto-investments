c8c63840b8f1a2d3ff2ae13e01b5c8a1109a3962
oren0e <countx@gmail.com>
2021-03-26 17:41:25 +0300

    Organize bot pool better

--- utils/telegram.py
+++ utils/telegram.py
@@ -8,6 +8,8 @@ from dataclasses import dataclass, field
 
 API_TOKEN: Optional[str] = os.environ.get('TELEGRAM_API_TOKEN')
 CHAT_ID: Optional[str] = os.environ.get('TELEGRAM_CHAT_ID')
+CGROUP_TOKEN: Optional[str] = os.environ.get("TELEGRAM_CGROUP_TOKEN")
+CGROUP_CHAT_ID: Optional[str] = os.environ.get("TELEGRAM_CGROUP_CHAT_ID")
 
 
 @dataclass
@@ -34,13 +36,14 @@ class BotPool:
     pool: Dict[str, Bot] = field(default_factory=dict)
 
     def add_bot(self, bot: Bot) -> None:
+        if not (bot.api_token and bot.chats):
+            raise RuntimeError("Missing Telegram token or chat id")
         self.pool[bot.name] = bot
 
 
-if API_TOKEN and CHAT_ID:
-    bot_pool = BotPool(pool={"cci_bot": Bot(name="cci_bot", api_token=API_TOKEN, chats={"cci_chat": Chat("cci_chat", chat_id=CHAT_ID)})})
-else:
-    raise RuntimeError("Missing Telegram token or chat id")
+bot_pool = BotPool()
+bot_pool.add_bot(Bot(name="cci_bot", api_token=API_TOKEN, chats={"cci_chat": Chat("cci_chat", chat_id=CHAT_ID)}))
+bot_pool.add_bot(Bot(name="cgroup_bot", api_token=CGROUP_TOKEN, chats={"cgroup_chat": Chat("cgroup_chat", chat_id=CGROUP_CHAT_ID)}))
 
 
 def telegram_send(bot: Bot, chat: str, message: str) -> None:
