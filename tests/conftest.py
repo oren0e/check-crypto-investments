938e65e2ef32603e3fd2473418378612168b375f
oren0e <countx@gmail.com>
2021-03-28 17:03:02 +0300

    Add conftest to use temp env vars for testing only

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import os
+import pytest
+
+try:
+    from .temp_env_vars import TEMP_ENV_VARS, ENV_VARS_TO_SUSPEND
+except ImportError:
+    TEMP_ENV_VARS = {}
+    ENV_VARS_TO_SUSPEND = []
+
+
+@pytest.fixture(scope="session", autouse=True)
+def tests_setup_and_teardown() -> None:
+    # Execute before the first test
+    old_environ = dict(os.environ)
+    os.environ.update(TEMP_ENV_VARS)
+    for env_var in ENV_VARS_TO_SUSPEND:
+        os.environ.pop(env_var, default=None)
+
+    yield
+    # Execute after the last test
+    os.environ.clear()
+    os.environ.update(old_environ)
