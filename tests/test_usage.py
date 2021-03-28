733372f473787405ab5481c5b0128f32fc12145d
oren0e <countx@gmail.com>
2021-03-28 16:03:37 +0300

    Add test for cgroup usage

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -1,5 +1,4 @@
-from utils.telegram import Bot, Chat
-from backend.data_tools import DataHandler, CoinSearch, CoinGeckoAPI
+from backend.data_tools import DataHandler, CoinSearch, CoinGeckoAPI, CGroupHandler
 from utils import s3_settings
 
 
@@ -19,6 +18,10 @@ def cs() -> CoinSearch:
 def dh() -> DataHandler:
     return DataHandler()
 
+@pytest.fixture
+def cgroup_handler() -> CGroupHandler:
+    return CGroupHandler()
+
 
 @mock.patch("backend.data_tools.bot_pool")
 @mock.patch("backend.data_tools.DataHandler.add_gas_to_msg")
@@ -37,6 +40,16 @@ def test_calculate_returns(mock_new_prices, tele_mock, initial_dict_mock, add_ga
     tele_mock.assert_called_with(bot_pool_mock.pool["cci_bot"], "cci_chat", sent_msg)
 
 
+@mock.patch("backend.data_tools.bot_pool")
+@mock.patch('backend.data_tools.telegram_send')
+@mock.patch("backend.data_tools.DataHandler.add_gas_to_msg")
+def test_cgroup_send_msg(add_gas_to_msg, tele_mock, bot_pool_mock, cgroup_handler) -> None:
+    sent_msg = f"*Gas:* 118 Gwei\n"
+    add_gas_to_msg.return_value = sent_msg
+    cgroup_handler.send_msg()
+    tele_mock.assert_called_with(bot_pool_mock.pool["cgroup_bot"], "cgroup_chat", sent_msg)
+
+
 @pytest.mark.integration
 def test_api_response() -> None:
     cg = CoinGeckoAPI()
@@ -44,6 +57,7 @@ def test_api_response() -> None:
     assert ans, "CoinGeckoAPI did not return a response!"
 
 
+@pytest.mark.integration
 def test_s3() -> None:
     s3_resource = s3_settings.session.resource('s3')
     body = s3_resource.Object(s3_settings.S3_BUCKET, "initial_investments").get()['Body'].read().decode('utf-8')
