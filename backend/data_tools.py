73c47d312cf95b8afb5934b58ca4eef666c04db9
oren0e <countx@gmail.com>
2021-05-01 20:11:06 +0300

    Refactor price logic and getting initial data to separate components

--- backend/data_tools.py
+++ backend/data_tools.py
@@ -2,11 +2,11 @@ from pycoingecko import CoinGeckoAPI
 from etherscan import Etherscan
 
 from typing import List, Dict, Optional
+import enum
 
 import os
 
-from config.common import COIN_LIST_FILEPATH
-from config.initial import initial_dict, ETHSCAN_API_KEY
+from config.common import COIN_LIST_FILEPATH, ROOT_DIR
 
 from utils.logger import logger
 from utils.telegram import telegram_send, Bot, Chat, BotPool
@@ -50,7 +50,75 @@ Everytime you'll want to add a bot you'll have to couple it with a dedicated han
 """
 
 
-class DataHandler:
+class DataSource(enum.Enum):
+    LOCAL = enum.auto()
+    REMOTE = enum.auto()
+
+
+class PriceHandler:
+    def __init__(self, initial_investments: Dict[str, float], vs_currency: str = 'usd') -> None:
+        self.vs_currency = vs_currency
+        self.cg = CoinGeckoAPI()
+        self.coin_ids: Optional[List[str]] = None
+        self.prices: Optional[Dict[str, float]] = None
+        self.initial_investments: Dict[str, float] = initial_investments
+
+    def get_prices(self) -> None:
+        """
+        Calculates dict with coin names
+        and their current prices
+        """
+        self.coin_ids = list(self.initial_investments.keys())
+        raw_prices: Dict[str, Dict[str, float]] = self.cg.get_price(ids=self.coin_ids, vs_currencies=self.vs_currency)
+        self.prices = {key: value[self.vs_currency] for key, value in raw_prices.items()}
+        logger.info(f'Got prices {self.prices}')
+
+    def calculate_returns(self) -> Dict[str, float]:
+        result_dict: Dict[str, float] = {}
+        for key, value in self.initial_investments.items():
+            result_dict[key] = round(((self.prices[key] - value) / value) * 100, 1)
+        logger.info(f"Got dictionary of returns: {result_dict}")
+        return result_dict
+
+
+class DataReader:
+    def __init__(self) -> None:
+        self.eth = Etherscan(os.environ.get('ETHSCAN_API_KEY'))
+
+    @staticmethod
+    def get_initial_investments_from_source(filename: str, source: DataSource = DataSource.LOCAL) -> Dict[str, float]:
+        if source == DataSource.LOCAL:
+            initial_inv_list = os.path.join(ROOT_DIR, 'lists', filename)
+            if os.path.exists(initial_inv_list):
+                with open(initial_inv_list, 'r') as f:
+                    initial_dict: Dict[str, float] = {line.strip().split()[0]: float(line.strip().split()[1]) for line in f
+                                                      if not line.startswith("#")}
+                logger.info("Initial investments read from file successfully")
+                return initial_dict
+            logger.warning("Initial_investments file was not found, using defaults instead.")
+            return {'bitcoin': 500, 'ethereum': 300}
+        elif source == DataSource.REMOTE:
+            s3_resource = s3_settings.session.resource('s3')
+            body = s3_resource.Object(s3_settings.S3_BUCKET, filename).get()['Body'].read().decode('utf-8')
+            return {line.strip().split()[0]: float(line.strip().split()[1]) for line in
+                                     body.split('\n') if not line.startswith("#")}
+        logger.error("Unsupported data source")
+        raise ValueError("Unsupported data source")
+
+    def get_gas_estimate(self) -> str:
+        return self.eth.get_gas_oracle()["ProposeGasPrice"]
+
+
+
+
+
+
+
+
+
+
+
+class DataHandler0:
 
     def __init__(self, vs_currency: str = 'usd', remote_initial: bool = False) -> None:
         self.coin_ids: Optional[List[str]] = None
