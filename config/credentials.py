4fcbb0a4bcce4eb0d719220600b4f9f51b4a14ff
oren0e <countx@gmail.com>
2021-05-14 19:49:17 +0300

    Add credentials parsing from config file or env var

---
+++ config/credentials.py
@@ -0,0 +1,19 @@
+import yaml
+import os
+
+from pathlib import Path
+from typing import Dict
+
+
+def parse_credentials(config_file_path: str = "config.yml") -> Dict[str, Dict[str, str]]:
+    config_path = Path(config_file_path)
+
+    if config_path.is_file():
+        with open('config.yml', 'r') as f:
+            config_dict = yaml.load(f, Loader=yaml.FullLoader)
+    else:
+        config_dict = yaml.safe_load(os.environ.get("CCI_CREDENTIALS"))
+
+    if all(key in config_dict for key in ["aws", "ethscan"]):
+        return config_dict
+    raise RuntimeError("Some essential keys are missing from config")
