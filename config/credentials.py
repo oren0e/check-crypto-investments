5c8681003607d80541bf650bea541c8b314264f9
oren0e <countx@gmail.com>
2021-06-22 23:10:38 +0300

    Change default to take credentials from env var

--- config/credentials.py
+++ config/credentials.py
@@ -2,18 +2,20 @@ import yaml
 import os
 
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Optional
 
 
-def parse_credentials(config_file_path: str = "config.yml") -> Dict[str, Dict[str, str]]:
-    config_path = Path(config_file_path)
+def parse_credentials(config_file_path: Optional[str] = None) -> Dict[str, Dict[str, str]]:
+    if config_file_path:
+        config_path = Path(config_file_path)
 
-    if config_path.is_file():
-        with open(config_path, 'r') as f:
-            config_dict = yaml.load(f, Loader=yaml.FullLoader)
+        if config_path.is_file():
+            with open(config_path, 'r') as f:
+                config_dict = yaml.load(f, Loader=yaml.FullLoader)
+        else:
+            raise ValueError(f"File {config_path} does not exist!")
     else:
         config_dict = yaml.safe_load(os.environ.get("CCI_CREDENTIALS"))
-
     if all(key in config_dict for key in ["aws", "ethscan"]):
         return config_dict
     raise RuntimeError("Some essential keys are missing from config")
