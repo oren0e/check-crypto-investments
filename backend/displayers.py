fde0b6cd002a5fb1527a8074aca495d7a9943966
oren0e <countx@gmail.com>
2021-06-24 00:50:31 +0300

    Change arguments to match the interface arguments

--- backend/displayers.py
+++ backend/displayers.py
@@ -4,15 +4,14 @@ from typing import Dict
 
 class GasDisplayer(DataDisplayer):
     @staticmethod
-    def generate(gas_price: str) -> str:
-        return f"*Gas:* {gas_price} Gwei\n"
+    def generate(data: str) -> str:
+        return f"*Gas:* {data} Gwei\n"
 
 
 class ReturnsDisplayer(DataDisplayer):
     @staticmethod
-    def generate(returns: Dict[str, float]) -> str:
+    def generate(data: Dict[str, float]) -> str:
         msg: str = ""
-        for key, value in returns.items():
+        for key, value in data.items():
             msg += f"Return for {key}: {value}%\n"
         return msg
-
