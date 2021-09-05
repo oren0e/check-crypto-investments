3ab041b886a547c1a6194fb7496a021c85efa760
oren0e <countx@gmail.com>
2021-09-06 02:27:07 +0300

    Add testing for initial investments parsing from env file

---
+++ tests/test_data.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+def test_correct_initial_investment_parsing() -> None:
+    with open("tests/data/fake_initial_investments") as f:
+        initial_investments = f.read()
+    initial_dict = {
+        line.strip().split()[0]: float(line.strip().split()[1])
+        for line in initial_investments.split("\n")
+        if not line.startswith("#")
+    }
+    assert len(initial_dict) == 2
+    assert initial_dict["asset_a"] == 100.0
+    assert initial_dict["asset_c"] == 300.0
+    with pytest.raises(KeyError):
+        initial_dict["asset_b"]
