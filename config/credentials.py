27f12f16b8e0c401f01f4a746a6647bc3f1aefdb
oren0e <countx@gmail.com>
2021-09-06 02:33:09 +0300

    Add credentials parsing

--- config/credentials.py
+++ config/credentials.py
@@ -1,8 +1,8 @@
-import yaml
 import os
-
 from pathlib import Path
 from typing import Dict, Optional
+
+import yaml
 
 
 def parse_credentials(config_file_path: Optional[str] = None) -> Dict[str, Dict[str, str]]:
@@ -12,10 +12,8 @@ def parse_credentials(config_file_path: Optional[str] = None) -> Dict[str, Dict[
         if config_path.is_file():
             with open(config_path, 'r') as f:
                 config_dict = yaml.load(f, Loader=yaml.FullLoader)
+                return config_dict
         else:
             raise ValueError(f"File {config_path} does not exist!")
     else:
-        config_dict = yaml.safe_load(os.environ.get("CCI_CREDENTIALS"))
-    if all(key in config_dict for key in ["aws", "ethscan"]):
-        return config_dict
-    raise RuntimeError("Some essential keys are missing from config")
+        return yaml.safe_load(os.environ.get("CCI_CREDENTIALS"))
