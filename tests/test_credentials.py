cd317af2f993ca53fba4ac3dd291334e39da349a
oren0e <countx@gmail.com>
2021-09-06 02:33:55 +0300

    Add tests for new design

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -1,13 +1,6 @@
-import pytest
-
 from config.credentials import parse_credentials
 
 
 def test_correct_config_file() -> None:
-    result_dict = parse_credentials('tests/data/correct_config.yml')
+    result_dict = parse_credentials("tests/data/correct_config.yml")
     assert len(result_dict) >= 2
-
-
-def test_wrong_config_file() -> None:
-    with pytest.raises(RuntimeError):
-        parse_credentials('tests/data/wrong_config.yml')
